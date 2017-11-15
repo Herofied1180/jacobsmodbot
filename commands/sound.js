9305f7449f11662b5023907f83eb744355e53cb8
Herofied1180 <noahsturner06@gmail.com>
2017-11-14 20:05:39 -0500

    Minor Bug Fixes

--- commands/sound.js
+++ commands/sound.js
@@ -12,7 +12,7 @@ function play(connection, message, sound) {
     //server.dispatcher = connection.playStream(YTDL('', {filter: "audioonly"}));
     message.channel.send('W.I.P Sound Effect')
   } else if (sound == "undertale") {
-    server.dispatcher = connection.playFile('../test.mp3', { seek: 0, volume: 1 });
+    server.dispatcher = connection.playFile(require('../test.mp3');
   } else {
     message.channel.send('Invalid Sound Effect');
   }
