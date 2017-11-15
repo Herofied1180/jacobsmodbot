6b36af2d5890c24bed3aa0a04e3e520602faa987
Herofied1180 <noahsturner06@gmail.com>
2017-11-14 20:59:50 -0500

    Bug Fixes

--- commands/sound.js
+++ commands/sound.js
@@ -7,14 +7,18 @@ function play(connection, message, sound) {
 
   if (sound == "wrong") {
     server.queue[0] = 'https://www.youtube.com/watch?v=RT3DirvAL-U&index=3&list=PLA_ZFp8wPEPBMbtlYW32cgqQtP549lwVa'
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}))
+      .then => {
+        connection.disconnect();
+      }
   } else if (sound == "troll") {
     //server.dispatcher = connection.playStream(YTDL('', {filter: "audioonly"}));
     message.channel.send('W.I.P Sound Effect')
   } else if (sound == "undertale") {
-    server.dispatcher = connection.playFile('test.mp3');
-  } else {
-    message.channel.send('Invalid Sound Effect');
+    server.dispatcher = connection.playFile('test.mp3')
+      .then => {
+        connection.disconnect();
+      }
   }
 
   server.queue.shift();
